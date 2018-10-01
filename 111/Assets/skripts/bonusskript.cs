7e8f269d957b9a7f33ae6e708250db639aa68298
Mariya Makhaeva <mahaeva.mariya@gmail.com>
2018-10-01 19:30:16 +0700

    first comit

---
+++ 111/Assets/skripts/bonusskript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class bonusskript : MonoBehaviour
+{
+    int c = 0;
+    int dist = 10;
+    float speed = 0.005f;
+
+    void FixedUpdate ()
+    {
+        c++;
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y + speed,
+            transform.position.z);
+        if (c > dist)
+        {
+            speed *= -1;
+            c = 0;
+        }
+       
+    }
+
+} 
---
+++ 111/Assets/skripts/nosatiicontrolll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nosatiicontrolll : MonoBehaviour {
+    public float speed = 20f;
+    private Rigidbody2D nos;
+
+    public GameObject bonus;
+
+	void Start () {
+        nos = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal");
+        nos.MovePosition(nos.position + Vector2.right * moveX * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        nos.AddForce(Vector2.up * 8000);
+
+    }
+} 
